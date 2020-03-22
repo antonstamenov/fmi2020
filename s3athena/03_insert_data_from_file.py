477e8b1bbc79a890e3633b8f50727dfd74741fdf
Anton Stamenov <antons@helecloud.com>
2020-03-22 15:13:02 +0200

    bad index!!! :)

--- s3athena/03_insert_data_from_file.py
+++ s3athena/03_insert_data_from_file.py
@@ -3,9 +3,7 @@ import json
 
 if __name__ == '__main__':
     s3 = boto3.client('s3', 'us-east-1')
-    id=1
     with open("books.json") as json_file:
         books = json.load(json_file)
-        for book in books:
-            id+=1
+        for id, book in enumerate(books, 2):
             s3.put_object(Bucket='gium', Key=f'books/id{id}', Body=json.dumps(book), ContentType='application/json')
